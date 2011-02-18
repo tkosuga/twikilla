806c32cee85ba07ed54826bd554c2d122e7cf570
terukazu kosuga <tkosuga@gmail.com>
2011-02-17 19:18:26 -0800

    Edited db/migrate/20100720161746_create_bots.rb via GitHub

--- db/migrate/20100720161746_create_bots.rb
+++ db/migrate/20100720161746_create_bots.rb
@@ -11,9 +11,7 @@ class CreateBots < ActiveRecord::Migration
       t.timestamps
     end
     
-    Bot.new(:name => "地域ッター西日本", :email => "tkosug.a@gmail.com", :password => "SFSa9ghD", :token => "76264861-GHp4PtPioBmPp90vxjBc6jMSkdyWSP6CnZgir1f3B", :secret => "YzqOuvczOosvv59K9ut9umYSH9hnGLU9tl0WQYDCum4").save!
-    Bot.new(:name => "地域ッター東日本", :email => "tkosu.ga@gmail.com", :password => "5o15YcCr", :token => "76265048-QjwVJGqjlylbLm8GXzG84RZaBmb9HdxwEf3hkdxUW", :secret => "a0n8NTIoJMIFfCPPDL8geztlSRv6PT57gvajWSSo").save!
-    
+
   end
   
   def self.down
